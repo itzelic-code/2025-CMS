181c86ebc74e87c8fbfb253801ad682dd7caf34b
itzelic-code <en3873@gmail.com>
2025-02-14 17:10:34 +0900

    setting: 부트2 다운그레이드 및 user-api모듈 생성

---
+++ user-api/src/test/java/com/itzelic/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzelic.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
